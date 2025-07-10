21ce27ca33c88a301ff7b4a287c64ca51ab350b1
Amarjeet Kumar <chauhanamarjeet116@gmail.com>
2025-07-10 19:10:41 +0530

    vaiable  restart learning

---
+++ variable/variable.js
@@ -0,0 +1,14 @@
+const accountId = 14444467;
+let accountEmail = "aj@gmail.com";
+var accountPassword  = "12345"
+
+accountCity = " Bihar"
+
+//  accountId =  3 // not allowed 
+accountEmail = "aj1@gmail.com";
+accountPassword = '67890'
+accountCity = 'delhi'
+
+
+
+console.table([accountId,accountEmail, accountPassword,accountCity])
